99d8006c0dae38238b0eaf8735ea5453fd826cd0
AryaStefhaniSinaga <hanpanka@gmail.com>
2025-05-06 18:39:44 +0700

    Add/edit data, save dan delate action,

---
+++ app/src/main/java/com/aryastefhani0140/miniproject2/ui/screen/DetailScreen.kt
@@ -0,0 +1,286 @@
+package com.aryastefhani0140.miniproject2.ui.screen
+
+import android.content.res.Configuration
+import android.widget.Toast
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.outlined.Check
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.aryastefhani0140.miniproject2.R
+import com.aryastefhani0140.miniproject2.ui.theme.Miniproject2Theme
+import com.aryastefhani0140.miniproject2.util.ViewModelFactory
+
+const val KEY_ID_TABUNGAN = "idTabungan"
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DetailScreen(navController: NavController, id: Long? = null) {
+    val context = LocalContext.current
+    val factory = ViewModelFactory(context)
+    val viewModel: DetailViewModel = viewModel(factory = factory)
+
+    var nama by remember { mutableStateOf("") }
+    var target by remember { mutableStateOf("") }
+    var tipe by remember { mutableStateOf("") }
+    var jumlah by remember { mutableStateOf("") }
+    var expanded by remember { mutableStateOf(false) }
+    var showDialog by remember { mutableStateOf(false) }
+
+    val tipeOptions = listOf(
+        stringResource(R.string.harian),
+        stringResource(R.string.mingguan),
+        stringResource(R.string.bulanan)
+    )
+
+    LaunchedEffect(Unit) {
+        if (id == null) return@LaunchedEffect
+        val data = viewModel.getTabungan(id) ?: return@LaunchedEffect
+        nama = data.nama
+        target = data.target.toString()
+        tipe = data.tipe
+        jumlah = data.jumlah.toString()
+    }
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                navigationIcon = {
+                    IconButton(onClick = { navController.popBackStack() }) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = stringResource(R.string.kembali),
+                            tint = MaterialTheme.colorScheme.primary
+                        )
+                    }
+                },
+                title = {
+                    if (id == null)
+                        Text(text = stringResource(id = R.string.tambah_tabungan))
+                    else
+                        Text(text = stringResource(id = R.string.edit_tabungan))
+                },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary
+                ),
+                actions = {
+                    IconButton(onClick = {
+                        if (nama.isEmpty() || target.isEmpty() || tipe.isEmpty() || jumlah.isEmpty()) {
+                            Toast.makeText(context, R.string.invalid, Toast.LENGTH_LONG).show()
+                            return@IconButton
+                        }
+
+                        try {
+                            val targetValue = target.toDouble()
+                            val jumlahValue = jumlah.toDouble()
+
+                            if (id == null) {
+                                viewModel.insert(nama, targetValue, tipe, jumlahValue)
+                            } else {
+                                viewModel.update(id, nama, targetValue, tipe, jumlahValue)
+                            }
+                            navController.popBackStack()
+                        } catch (e: NumberFormatException) {
+                            Toast.makeText(context, R.string.invalid, Toast.LENGTH_LONG).show()
+                        }
+                    }) {
+                        Icon(
+                            imageVector = Icons.Outlined.Check,
+                            contentDescription = stringResource(R.string.simpan),
+                            tint = MaterialTheme.colorScheme.primary
+                        )
+                    }
+                    if (id != null) {
+                        DeleteAction {
+                            showDialog = true
+                        }
+                    }
+                }
+            )
+        }
+    ) { padding ->
+        FormTabungan(
+            nama = nama,
+            onNamaChange = { nama = it },
+            target = target,
+            onTargetChange = { target = it },
+            tipe = tipe,
+            onTipeChange = { tipe = it },
+            tipeOptions = tipeOptions,
+            expanded = expanded,
+            onExpandedChange = { expanded = it },
+            jumlah = jumlah,
+            onJumlahChange = { jumlah = it },
+            modifier = Modifier.padding(padding)
+        )
+    }
+
+    if (id != null && showDialog) {
+        DisplayAlertDialog(
+            onDismissRequest = { showDialog = false }
+        ) {
+            showDialog = false
+            viewModel.delete(id)
+            navController.popBackStack()
+        }
+    }
+}
+
+@Composable
+fun DeleteAction(delete: () -> Unit) {
+    var expanded by remember { mutableStateOf(false) }
+
+    IconButton(onClick = { expanded = true }) {
+        Icon(
+            imageVector = Icons.Filled.MoreVert,
+            contentDescription = stringResource(R.string.lainnya),
+            tint = MaterialTheme.colorScheme.primary
+        )
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false }
+        ) {
+            DropdownMenuItem(
+                text = {
+                    Text(text = stringResource(id = R.string.hapus))
+                },
+                onClick = {
+                    expanded = false
+                    delete()
+                }
+            )
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormTabungan(
+    nama: String, onNamaChange: (String) -> Unit,
+    target: String, onTargetChange: (String) -> Unit,
+    tipe: String, onTipeChange: (String) -> Unit,
+    tipeOptions: List<String>,
+    expanded: Boolean, onExpandedChange: (Boolean) -> Unit,
+    jumlah: String, onJumlahChange: (String) -> Unit,
+    modifier: Modifier
+) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        OutlinedTextField(
+            value = nama,
+            onValueChange = { onNamaChange(it) },
+            label = { Text(text = stringResource(R.string.nama_tabungan)) },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(
+                capitalization = KeyboardCapitalization.Words,
+                imeAction = ImeAction.Next
+            ),
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        OutlinedTextField(
+            value = target,
+            onValueChange = { onTargetChange(it) },
+            label = { Text(text = stringResource(R.string.target)) },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Next
+            ),
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        ExposedDropdownMenuBox(
+            expanded = expanded,
+            onExpandedChange = { onExpandedChange(it) },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            OutlinedTextField(
+                value = tipe,
+                onValueChange = {},
+                readOnly = true,
+                label = { Text(text = stringResource(R.string.tipe)) },
+                trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
+                modifier = Modifier
+                    .menuAnchor()
+                    .fillMaxWidth()
+            )
+
+            ExposedDropdownMenu(
+                expanded = expanded,
+                onDismissRequest = { onExpandedChange(false) }
+            ) {
+                tipeOptions.forEach { option ->
+                    DropdownMenuItem(
+                        text = { Text(text = option) },
+                        onClick = {
+                            onTipeChange(option)
+                            onExpandedChange(false)
+                        }
+                    )
+                }
+            }
+        }
+
+        OutlinedTextField(
+            value = jumlah,
+            onValueChange = { onJumlahChange(it) },
+            label = { Text(text = stringResource(R.string.jumlah_menabung)) },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Done
+            ),
+            modifier = Modifier.fillMaxWidth()
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
+@Composable
+fun DetailScreenPreview() {
+    Miniproject2Theme {
+        DetailScreen(rememberNavController())
+    }
+}
