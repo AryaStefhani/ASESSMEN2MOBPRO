52b612db940aac72f26e55905c98056bdfd617a4
AryaStefhaniSinaga <hanpanka@gmail.com>
2025-05-06 18:20:16 +0700

    Membuat navigasi aplikasi

---
+++ app/src/main/java/com/aryastefhani0140/miniproject2/navigation/Screen.kt
@@ -0,0 +1,11 @@
+package com.aryastefhani0140.miniproject2.navigation
+
+import com.aryastefhani0140.miniproject2.ui.screen.KEY_ID_TABUNGAN
+
+sealed class Screen(val route: String) {
+    data object Home : Screen("mainScreen")
+    data object FormBaru : Screen("detailScreen")
+    data object FormUbah : Screen("detailScreen/{$KEY_ID_TABUNGAN}") {
+        fun withId(id: Long) = "detailScreen/$id"
+    }
+}
