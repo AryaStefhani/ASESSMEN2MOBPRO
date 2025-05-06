f4d7d37f805764eb8bf5f4ce9a79a9e977833848
AryaStefhaniSinaga <hanpanka@gmail.com>
2025-05-06 18:40:55 +0700

    Membuat ViewModelFactory

---
+++ app/src/main/java/com/aryastefhani0140/miniproject2/util/ViewModelFactory.kt
@@ -0,0 +1,23 @@
+package com.aryastefhani0140.miniproject2.util
+
+import android.content.Context
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.aryastefhani0140.miniproject2.database.TabunganDb
+import com.aryastefhani0140.miniproject2.ui.screen.DetailViewModel
+import com.aryastefhani0140.miniproject2.ui.screen.MainViewModel
+
+class ViewModelFactory(
+    private val context: Context
+) : ViewModelProvider.Factory {
+    @Suppress("unchecked_cast")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        val dao = TabunganDb.getInstance(context).dao
+        if (modelClass.isAssignableFrom(MainViewModel::class.java)) {
+            return MainViewModel(dao) as T
+        } else if (modelClass.isAssignableFrom(DetailViewModel::class.java)) {
+            return DetailViewModel(dao) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
